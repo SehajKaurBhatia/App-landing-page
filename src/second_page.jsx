5904d3d1a17f108d9f7f4ca944ed2a4a0f80e91f
Sehaj <114878033+SehajKaurBhatia@users.noreply.github.com>
2024-06-03 20:08:43 +0530

    Update second_page.jsx

--- src/second_page.jsx
+++ src/second_page.jsx
@@ -7,13 +7,13 @@ return(
     <img className="rings2"src="/images/rings2.png"/>
     <img className="el3"src="/images/el3.png"/>
     </div>
-    <img className="star4"src="/images/star 3.png"/>
+    <img className="star4"src="/images/Star 3.png"/>
     <div className='box2'>
     <p className='features'>FEATURES</p>
     <h1>Uifry Premium</h1>
     <p className='heading'><img className="icon5" src="/images/star2.png"/>Budgeting Intervals</p>
     <p className='text'>Cum Et Convallis Risus Placerat Aliquam, Nunc. Scelerisque Aliquet<br/> Faucibus Tincidunt Eu Adipiscing Sociis Arcu Lorem Porttitor.</p>
-    <p className='heading'><img className="icon5" src="/images/icon.png"/>Budgeting Intervals</p>
+    <p className='heading'><img className="icon5" src="/images/Icon.png"/>Budgeting Intervals</p>
     <p className='text'>Cum Et Convallis Risus Placerat Aliquam, Nunc. Scelerisque Aliquet<br/> Faucibus Tincidunt Eu Adipiscing Sociis Arcu Lorem Porttitor.</p>
     <p className='heading'><img className="icon5" src="/mages/cube.png"/>Budgeting Intervals</p>
     <p className='text'>Cum Et Convallis Risus Placerat Aliquam, Nunc. Scelerisque Aliquet<br/> Faucibus Tincidunt Eu Adipiscing Sociis Arcu Lorem Porttitor.</p>
