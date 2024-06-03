10c61a67f3b90327c014aa5fb955b660b98f671f
Sehaj <114878033+SehajKaurBhatia@users.noreply.github.com>
2024-06-03 20:02:51 +0530

    Update Fourth_Page.jsx

--- src/Fourth_Page.jsx
+++ src/Fourth_Page.jsx
@@ -3,17 +3,17 @@ const Fourth=()=>{
 return(
     <>
     <div className="box5">
-        <img className="phone6" src="/public/images/phone4.png"/>
-        <img className="rings5" src="/public/images/rings3.png"/>
-        <img className="el7" src="/public/images/Group 35932 (1).png"/>
+        <img className="phone6" src="/images/phone4.png"/>
+        <img className="rings5" src="/images/rings3.png"/>
+        <img className="el7" src="/images/Group 35932 (1).png"/>
     </div>
 
     <div className='box6'>
-        <p className='heading3'><img className="icon4" src="/public/images/star1.png"/><img className="icon3" src="/public/images/Ellipse 2158.png"/>Fully customizable</p>
+        <p className='heading3'><img className="icon4" src="/images/star1.png"/><img className="icon3" src="/images/Ellipse 2158.png"/>Fully customizable</p>
         <p className='txt2'>Arcu at dictum sapien, mollis. Vulputate sit id accumsan,<br/> ultricies. In ultrices malesuada elit mauris etiam odio. Duis<br/> tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus,<br/> diam suspendisse sit nunc, gravida eu. Lectus eget eget ac<br/> dolor neque lorem sapien, suspendisse aliquam.</p>
     </div>
-    <img className="star6"src="/public/images/star 3.png"/>
-    <img className="star7"src="/public/images/star 1.png"/>
+    <img className="star6"src="/images/star 3.png"/>
+    <img className="star7"src="/images/star 1.png"/>
     </>
 )
 }
