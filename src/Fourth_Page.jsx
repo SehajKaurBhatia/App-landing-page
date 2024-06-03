aee99069d2c7d4a11059aaa9dadf5e27ce97f02b
Sehaj <114878033+SehajKaurBhatia@users.noreply.github.com>
2024-06-03 20:10:35 +0530

    Update Fourth_Page.jsx

--- src/Fourth_Page.jsx
+++ src/Fourth_Page.jsx
@@ -12,8 +12,8 @@ return(
         <p className='heading3'><img className="icon4" src="/images/star1.png"/><img className="icon3" src="/images/Ellipse 2158.png"/>Fully customizable</p>
         <p className='txt2'>Arcu at dictum sapien, mollis. Vulputate sit id accumsan,<br/> ultricies. In ultrices malesuada elit mauris etiam odio. Duis<br/> tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus,<br/> diam suspendisse sit nunc, gravida eu. Lectus eget eget ac<br/> dolor neque lorem sapien, suspendisse aliquam.</p>
     </div>
-    <img className="star6"src="/images/star 3.png"/>
-    <img className="star7"src="/images/star 1.png"/>
+    <img className="star6"src="/images/Star 3.png"/>
+    <img className="star7"src="/images/Star 1.png"/>
     </>
 )
 }
